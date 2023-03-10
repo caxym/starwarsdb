4984db722369591cd6459d23a63c8837c32afa0a
raul araque <luis.raul.araque2@gmail.com>
2023-03-10 17:58:15 -0500

    db StarWars flask+python

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask, request, jsonify
+from models import db, User, Favorito, Personaje, Vehiculo, Planeta
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///starwars.db'
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return "Base de Datos StarWars"
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    user = User()
+    user.name = request.json.get('name')
+    user.lastname = request.json.get('lastname')
+    user.username = request.json.get('username')
+    user.email = request.json.get('email')
+    user.password = request.json.get('password')
+
+    db.session.add(user)
+    db.session.commit()
+
+    return "usuario guardado"
+
+@app.route('/pj', methods=['POST'])
+def create_pj():
+    personaje = Personaje()
+    personaje.image = request.json.get('image')
+    personaje.name = request.json.get('name')
+    personaje.species = request.json.get('species')
+    personaje.gender = request.json.get('gender')
+    personaje.homeworld = request.json.get('homeworld')
+    personaje.height = request.json.get('height')
+    personaje.mass = request.json.get('mass')
+    personaje.bornLocation = request.json.get('bornLocation')
+    personaje.deadLocation = request.json.get('deadLocation')
+
+    db.session.add(personaje)
+    db.session.commit()
+
+    return "personaje guardado"
+
+@app.route('/ship', methods=['POST'])
+def create_ship():
+    vehiculo = Vehiculo()
+    vehiculo.image = request.json.get('image')
+    vehiculo.name = request.json.get('name')
+    vehiculo.model = request.json.get('model')
+    vehiculo.passengers = request.json.get('passengers')
+    vehiculo.cargo_capacity = request.json.get('cargo_capacity')
+    vehiculo.crew = request.json.get('crew')
+    vehiculo.length = request.json.get('length')
+    vehiculo.starship_class = request.json.get('starship_class')
+    vehiculo.cost_in_credits = request.json.get('cost_in_credits')
+
+    db.session.add(vehiculo)
+    db.session.commit()
+
+    return "Vehiculo guardado"
+
+
+@app.route('/planet', methods=['POST'])
+def create_planet():
+    planeta = Planeta()
+    planeta.image = request.json.get('image')
+    planeta.name = request.json.get('name')
+    planeta.climate = request.json.get('climate')
+    planeta.terrain = request.json.get('terrain')
+    planeta.population = request.json.get('population')
+    planeta.diameter = request.json.get('diameter')
+    planeta.gravity = request.json.get('gravity')
+    planeta.rotation_period = request.json.get('rotation_period')
+    planeta.surface_water = request.json.get('surface_water')
+
+    db.session.add(planeta)
+    db.session.commit()
+
+    return "Planeta guardado"
+
+@app.route('/users/list', methods=['GET'])
+def get_list_user():
+    users = User.query.all()
+    result = []
+
+    for user in users:
+        result.append(user.user_list())
+    return jsonify(result)
+
+@app.route('/pj/list', methods=['GET'])
+def get_list_pj():
+    personajes = Personaje.query.all()
+    result = []
+
+    for personaje in personajes:
+        result.append(personaje.user_personaje())
+    return jsonify(result)
+
+@app.route('/ship/list', methods=['GET'])
+def get_list_ship():
+    vehiculos = Vehiculo.query.all()
+    result = []
+
+    for vehiculo in vehiculos:
+        result.append(vehiculo.user_vehiculo())
+    return jsonify(result)
+
+@app.route('/planet/list', methods=['GET'])
+def get_list_planet():
+    planets = Planeta.query.all()
+    result = []
+
+    for planet in planets:
+        result.append(planet.user_planeta())
+    return jsonify(result)
+
+@app.route('/users/<int:id>', methods=['PUT','DELETE'])
+def update_user(id):
+    user = User.query.get(id)
+    if user is not None:
+        if request.method =='DELETE':
+            db.session.delete(user)
+            db.session.commit()
+
+            return 204
+        else:
+            user.username = request.json.get('username')
+
+            db.session.commit()
+
+            return jsonify('Usuario Actualizado')
+    
+    return jsonify('Usuario no encontrado')
+
+with app.app_context():
+    db.create_all()
+
+app.run(host='localhost', port='5000')
---
+++ models.py
@@ -0,0 +1,122 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50),nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50),nullable=False)
+    email = db.Column(db.String(50),nullable=False)
+    password = db.Column(db.String(50),nullable=False)
+    favorito = db.relationship('Favorito')
+
+    def user_list(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'lastname': self.lastname,
+            'username': self.username,
+            'email': self.email
+        }
+
+
+class Favorito(db.Model):
+    __tablename__ = 'favorito'
+    id = db.Column(db.Integer, primary_key=True)
+    name_personajes = db.Column(db.String(50),ForeignKey('personaje.name'))
+    name_vehiculos = db.Column(db.String(50),ForeignKey('vehiculo.name'))
+    name_planetas = db.Column(db.String(50),ForeignKey('planeta.name'))
+    user_email = db.Column(db.String(50),ForeignKey('user.email'))
+
+    def user_favorite(self):
+        return{
+            'id': self.id,
+            'personajes': self.personajes,
+            'vehiculos': self.vehiculos,
+            'planetas': self.planetas
+        }
+
+class Personaje(db.Model):
+    __tablename__ = 'personaje'
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(50),nullable=False)
+    species = db.Column(db.String(50),nullable=False)
+    gender = db.Column(db.String(50),nullable=False)
+    homeworld = db.Column(db.String(50),nullable=False)
+    height = db.Column(db.String(50),nullable=False)
+    mass = db.Column(db.String(50),nullable=False)
+    bornLocation = db.Column(db.String(50),nullable=False)
+    deadLocation = db.Column(db.String(50),nullable=False)
+    favorito = db.relationship('Favorito')
+
+    def user_personaje(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'species': self.species,
+            'gender': self.gender,
+            'homeworld': self.homeworld,
+            'height': self.height,
+            'mass': self.mass,
+            'bornLocation': self.bornLocation,
+            'deadLocation': self.deadLocation
+        }
+
+class Vehiculo(db.Model):
+    __tablename__ = 'vehiculo'
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(50),nullable=False)
+    model = db.Column(db.String(50),nullable=False)
+    passengers = db.Column(db.String(50),nullable=False)
+    cargo_capacity = db.Column(db.String(50),nullable=False)
+    crew = db.Column(db.String(50),nullable=False)
+    length = db.Column(db.String(50),nullable=False)
+    starship_class = db.Column(db.String(50),nullable=False)
+    cost_in_credits = db.Column(db.String(50),nullable=False)
+    favorito = db.relationship('Favorito')
+
+    def user_vehiculo(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'model': self.model,
+            'passengers': self.passengers,
+            'cargo_capacity': self.cargo_capacity,
+            'crew': self.crew,
+            'length': self.length,
+            'starship_class': self.starship_class,
+            'cost_in_credits': self.cost_in_credits
+        }
+
+class Planeta(db.Model):
+    __tablename__ = 'planeta'
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(50),nullable=False)
+    climate = db.Column(db.String(50),nullable=False)
+    terrain = db.Column(db.String(50),nullable=False)
+    population = db.Column(db.String(50),nullable=False)
+    diameter = db.Column(db.String(50),nullable=False)
+    gravity = db.Column(db.String(50),nullable=False)
+    rotation_period = db.Column(db.String(50),nullable=False)
+    surface_water = db.Column(db.String(50),nullable=False)
+    favorito = db.relationship('Favorito')
+
+    def user_planeta(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'climate': self.climate,
+            'terrain': self.terrain,
+            'population': self.population,
+            'diameter': self.diameter,
+            'gravity': self.gravity,
+            'rotation_period': self.rotation_period,
+            'surface_water': self.surface_water
+        }
